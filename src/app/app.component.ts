57c441cee762e8343cda8a45afaceef55e5acdd6
RafaAps <rafaelaaposantos@gmail.com>
2025-02-21 15:00:22 -0300

    aula1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+//decorar a classe 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form', //!é atravez do selctor q é possivel usar e montar o componente
+  standalone: false,
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+
+}
